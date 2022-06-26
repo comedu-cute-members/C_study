88e6041ccbfbce2d0bef7effaff02a682251db11
이성준 <104264447+awseiorubhawuiolebfrdfgs@users.noreply.github.com>
2022-06-26 14:34:45 +0900

    Add files via upload

---
+++ awseiorubhawuiolebfrdfgs/week1_2588.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a*((b%100)%10) );
+    printf("%d\n", a*((b%100)/10) );
+    printf("%d\n", a*(b/100) );
+    printf("%d", a*b );
+    return 0;
+      
+}
