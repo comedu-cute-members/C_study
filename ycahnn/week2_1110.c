19a2ced04eb5a74025fccd6e6afe487cb7747f82
ycahnn <104204689+ycahnn@users.noreply.github.com>
2022-06-28 20:33:22 +0900

    Update week2_1110.c

--- ycahnn/week2_1110.c
+++ ycahnn/week2_1110.c
@@ -1,1 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int main()
+{
+	int i=0,n, A, B, result=-1;
+	scanf("%d", &A);
+		
+	
+	while (A != result)
+	{
+		if (i == 0)
+			result = A;
+
+		B = result / 10 + (result % 10);
+		result = (result % 10) * 10 + B%10;
+		i++;
+	} 
+
+	printf("%d", i);
+
+	return 0;
+
+}
