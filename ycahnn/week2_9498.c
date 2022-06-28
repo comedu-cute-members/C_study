6523e74247c9f88ee280f592c1ff6422e9fc5ab1
ycahnn <104204689+ycahnn@users.noreply.github.com>
2022-06-28 21:52:25 +0900

    Update week2_9498.c

--- ycahnn/week2_9498.c
+++ ycahnn/week2_9498.c
@@ -6,21 +6,33 @@ int main(void)
 {
 	int score;
 	scanf("%d", &score);
-
-	if (score >= 90 && score <= 100)
-		printf("A");
-
-	else if (score >= 80 && score <= 89)
-		printf("B");
-
-	else if (score >= 70 && score <= 79)
-		printf("C");
-
-	else if (score >= 60 && score <= 69)
-		printf("D");
-
-	else
+	if (score < 60)
 		printf("F");
+	else
+	{
+		switch (score / 10)
+		{
+		case 10:
+			printf("A");
+			break;
 
-return 0;
+		case 9:
+			printf("A");
+			break;
+
+		case 8:
+			printf("B");
+			break;
+
+		case 7:
+			printf("C");
+			break;
+
+		case 6:
+			printf("D");
+			break;
+		}
+	}
+
+	return 0;
 }
