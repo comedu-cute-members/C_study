f4fdf98632ac8970c03662badf8d3f68439ce0a0
yoohoo <103162528+jeazure11@users.noreply.github.com>
2022-06-28 13:25:26 +0900

    Update week2_9498.c

--- yoohoo/week2_9498.c
+++ yoohoo/week2_9498.c
@@ -2,20 +2,12 @@
 int main(){
     int a;
     scanf("%d",&a);
-    if (a >= 90 && a<= 100){
-        printf("A");
-    }
-    else if (a >= 80 && a <= 89){
-        printf("B");
-    }
-    else if (a >= 70 && a <= 79){
-        printf("C");
-    } 
-    else if (a >=60 && a <= 69){
-        printf("D");
-    }
-    else{
-        printf("F");
+    switch(a){
+        case 90 ... 100:printf("A"); break;
+        case 80 ... 89:printf("B"); break;
+        case 70 ... 79:printf("C"); break;
+        case 60 ... 69:printf("D"); break;
+        case 0 ...59:printf("F"); break;
     }
     return 0;
 }
