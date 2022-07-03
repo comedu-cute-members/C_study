688e5f6fa92c9f8c7b0bb7d09f66ffd4b0156086
이성준 <104264447+awseiorubhawuiolebfrdfgs@users.noreply.github.com>
2022-07-03 13:02:14 +0900

    Add files via upload

---
+++ awseiorubhawuiolebfrdfgs/week2_1110.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main() 
+{
+    int ten = 0;
+    int one = 0;
+    int sum = 0;
+    int n = 0;
+    int a = 0;
+    int p = 1;
+    
+    scanf("%d", &a);
+    n = a;
+
+    while (a != 0)
+    {
+        ten = n/10;
+        one = n % 10;
+        sum = ten + one;
+        n = (one * 10) + (sum % 10);
+        if (n==a)
+        {
+            break;
+        }
+        p++;
+    }
+    printf("%d", p);
+    return (0);
+}
