c424f381e3a934bcb3c87b747303e2ad14e09e59
Shin714 <107873352+Shin714@users.noreply.github.com>
2022-07-03 20:24:44 +0900

    Create week2_9498.c

---
+++ Shin714/week2_9498.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main_baekjoon9498() {
+	int score;
+	scanf("%d", &score);
+	switch (score/10) {
+	case 10:
+	case 9:
+		printf("A");
+		break;
+	case 8:
+		printf("B");
+		break;
+	case 7:
+		printf("C");
+		break;
+	case 6:
+		printf("D");
+		break;
+	default:
+		printf("F");
+		break;
+	}
+	return 0;
+}
