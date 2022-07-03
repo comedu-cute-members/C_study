fd7d5b69abc4865a73df05c4b98f535069b0c66a
ahin0119 <107998015+ahin0119@users.noreply.github.com>
2022-07-03 18:28:07 +0900

    Create week2_2480.c

---
+++ ahin/week2_2480.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int a, b, c;
+
+	scanf("%d %d %d", &a,&b,&c);
+
+	if (a == b && b == c)
+	{
+		printf("%d", 10000 + a * 1000);
+	}
+	else if (a == b && b != c)
+	{
+		printf("%d", 1000 + a * 100);
+	}
+	else if (b == c && a != c)
+	{
+		printf("%d", 1000 + b * 100);
+	}
+	else if (a == c && b != c)
+	{
+		printf("%d", 1000 + a * 100);
+	}
+	else
+	{
+		if (a > b && a > c)
+		{
+			printf("%d", a * 100);
+		}
+		else if (b > a && b > c)
+		{
+			printf("%d", b * 100);
+		}
+		else if (c > a && c > b)
+		{
+			printf("%d", c * 100);
+		}
+	}
+return 0;
+}
