8c54f631afef54238444aed802a16a9730b91755
Shin714 <107873352+Shin714@users.noreply.github.com>
2022-07-03 20:21:42 +0900

    Create week1_10952.c

---
+++ Shin714/week1_10952.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int A, B;
+	scanf("%d %d", &A, &B);
+	while (A != 0 && B != 0) {
+		printf("%d\n", A + B);
+		scanf("%d %d", &A, &B);
+	}
+	return 0;
+}
