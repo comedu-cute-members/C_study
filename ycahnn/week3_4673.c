155a199b51384fb718f9411a9572dbcb1271c24f
ycahnn <104204689+ycahnn@users.noreply.github.com>
2022-07-08 01:03:44 +0900

    Create week3_4673.c

---
+++ ycahnn/week3_4673.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int d(n)    
+{
+	int result=n;
+
+	while (n!= 0)
+	{
+		result += n % 10;
+		n=n / 10;
+	}
+	return result;
+}
+
+int main()
+{
+	
+	int num, i, self=1;
+	for (num = 1; num <=10000; num++)  
+	{
+		for (i = 1; i <= num; i++)		
+		{
+			if (d(i) == num)
+			self = 0;
+		}
+		if (self == 1)
+		{
+			printf("%d\n", num);
+		}
+		self = 1;
+	}
+
+	return 0;
+}
