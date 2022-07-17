9beb136a6128e95374bb6a145b9274feaec35310
이성준 <104264447+awseiorubhawuiolebfrdfgs@users.noreply.github.com>
2022-07-17 21:17:22 +0900

    Update week4_2750.c

--- Shin714/week4_2750.c
+++ Shin714/week4_2750.c
@@ -1,26 +1,23 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main() {
-	int arr[1000] = { 0, };
-	int temp, count;
-	scanf("%d", &count);
-
-	for (int i = 0; i < count; i++) {
-		scanf("%d", &arr[i]);
-	}
-
-
-
-	for (int i = 0; i < count - 1; i++) {
-		int j = i;
-		while (j >= 0 && arr[j] > arr[j + 1]) {
-			temp = arr[j];
-			arr[j] = arr[j + 1];
-			arr[j + 1] = temp;
-			j--;
-		}
-	}
-	for (int i = 0; i < count; i++) printf("%d\n", arr[i]);
-
+int main(){
+    int n;
+    int temp;
+    scanf("%d\n", &n);
+    int arr[1000]={0,};
+    for(int i=0;i<n;i++){
+        scanf("%d\n", &arr[i]);
+    }
+    
+        for(int j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+  
+    for(int i=0;i<n;i++){
+        printf("%d\n", arr[i]);
+    }
 }
