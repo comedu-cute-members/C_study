3d1d24d244fba58ff237e3974daef33b055a6b96
이성준 <104264447+awseiorubhawuiolebfrdfgs@users.noreply.github.com>
2022-07-17 21:17:44 +0900

    Update week4_2750.c

--- awseiorubhawuiolebfrdfgs/week4_2750.c
+++ awseiorubhawuiolebfrdfgs/week4_2750.c
@@ -1,39 +1,23 @@
-# include <stdio.h>
-
-int* insert_sort(int arr[], int len){
-   for (int i=1; i<len; i++){
-        int number = arr[i];
-        for (int j=i-1; j>=0; j--){
-            int tmp = arr[j];
-            if (tmp > number){
-                arr[j+1] = tmp;
-                arr[j] = number;
-            }
-            else{
-                arr[j+1] = number;
-                break;
-            }
-        }
-   }
-   return arr;
-}
+#include <stdio.h>
 
 int main(){
     int n;
-    scanf("%d", &n);
+    int temp;
+    scanf("%d\n", &n);
+    int arr[1000]={0,};
+    for(int i=0;i<n;i++){
+        scanf("%d\n", &arr[i]);
+    }
     
-    int arr[n];
-    for (int i=0; i<n; i++){
-        int num;
-        scanf("%d", &num);
-        arr[i] = num;
+        for(int j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+  
+    for(int i=0;i<n;i++){
+        printf("%d\n", arr[i]);
     }
-
-    int* sorted =  insert_sort(arr, n);
-
-    for (int j=0; j<n; j++){
-        printf("%d\n", arr[j]);
-    }
-
-    return 0;
 }
