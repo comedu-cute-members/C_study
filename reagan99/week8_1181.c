8fce2d03bce166dc5ef06a78395439c2d3d847dd
reagan99 <104223092+reagan99@users.noreply.github.com>
2022-08-16 23:43:50 +0900

    Update week8_1181.c

--- reagan99/week8_1181.c
+++ reagan99/week8_1181.c
@@ -1,42 +1,56 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 int main(int argc, char** argv) {
-	int a;
-	char temp1[51], temp2[51];
-	int len;
-	scanf("%d", &a);
-	int n=1;
-	char word[a][51];
-	char word2[a][51];
-	for (int i=0;i<a;i++){
-		scanf("%s",&word[i]);
-	}
-
+	char word[1000000];
+	char a='A';
+	int k=0;
+	int m=0;
+	int cnt2[26]={0};
+	int cnt3[27]={0};
+	char cnt[26]={0};
+	int temp;
 	
-	for(int i=0;i<a-1;i++){ //알파벳 정렬  
-		for(int j=i+1;j<a;j++){
-			if(strcmp(word[i],word[j])>0){
-				strcpy(temp1, word[i]);
-				strcpy(word[i],word[j]);
-				strcpy(word[j],temp1);
-			}
-		}
-	}
-
-	for(int i=0; i<a;i++){//길이 정렬  
-		for(int j=0; j<a-i;j++){
-			if(strlen(word[j])>strlen(word[j+1])){
-				strcpy(temp2, word[j]);
-				strcpy(word[j],word[j+1]);
-				strcpy(word[j+1],temp2);
-			}
-		}
+	scanf("%s",word);
+	int len = strlen(word);
+	for(int i=0;i<26;i++){ //  
+		cnt[i]=a;
+		a++;
+	} 
+	for(int i=0; i<strlen(word);i++){
+		if(int(word[i])>=97) word[i]= int(word[i])-32;//전부대문자로
 	}
 	
-	for(int i=0; i<a+1;i++){
-		if(strcmp(word[i],word[i+1])==0) continue;
-		printf("%s\n",word[i]);
+	for(int i=0; i<26;i++){
+		for(int j=0;j<len;j++){
+			if(cnt[i]==word[j]){
+				cnt2[i]=++k;
+				cnt3[i]=++m;
+			} 
+		}
+		k=0;
+		m=0;
+	}
+    for (int i = 0; i < 26; i++){
+        for (int j = 0; j < 25; j++){   
+            if (cnt2[j] < cnt2[j + 1]){          
+                temp = cnt2[j];
+                cnt2[j] = cnt2[j + 1];
+                cnt2[j + 1] = temp;           
+            }
+        }
+    }
+    
+	if(cnt2[0]==cnt2[1]){
+		printf("?");
+	}
+	else{
+		for(int i=0;i<26;i++){
+			if(cnt3[i]==cnt2[0]){
+				printf("%c",i+65);
+			}
+		}
 	}
 	
 	return 0;
