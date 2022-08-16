3933c8febf4dc44d5ec104006dbaa5a0c2b9c63a
JS <104408868+captaintimoo@users.noreply.github.com>
2022-08-16 22:26:13 +0900

    Create week8_14681.c

---
+++ captaintimoo/week8_14681.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+int x, y;
+scanf("%d %d", &x, &y);
+if (x > 0 && y > 0)
+printf("1");
+if (x < 0 && y > 0)
+printf("2");
+if (x < 0 && y < 0)
+printf("3");
+if (x > 0 && y < 0)
+printf("4");
+}
